660e145256e5ab47f6a5a1632a5008e48c3c4bf3
lbias <306508354@qq.com>
2017-08-05 07:18:38 +0800

    controller lifts#create

--- app/controllers/lifts_controller.rb
+++ app/controllers/lifts_controller.rb
@@ -2,4 +2,19 @@ class LiftsController < ApplicationController
   def index
     @lifts = Lift.all
   end
+
+  def create
+    @lift = Lift.new(lift_params)
+    if @lift.save
+      render json: @lift
+    else
+      render json: @lift.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def lift_params
+    params.require(:lift).permit(:date, :liftname, :ismetric, :weightlifted, :resperformed, :onerm)
+  end
 end
